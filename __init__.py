80e4c8206d3590b36459ef20eef67a0f38ec39dc
Ian Glen Neal <ian.gl.neal@gmail.com>
2016-07-29 14:21:52 -0700

    Trying more stuff to get travis to work

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 __all__ = ["mock", "robinhood", "tests"]
+import os, sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 import robinhood
 from robinhood import RobinhoodTrader
 import mock
