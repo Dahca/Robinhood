630fd912cfc6e4d057a5c6fa8771bba964173287
Ian Glen Neal <ian.gl.neal@gmail.com>
2016-07-30 18:43:22 -0700

    Renaming mock to something more specific

--- robinhood/__init__.py
+++ robinhood/__init__.py
@@ -1,7 +1,7 @@
-__all__ = ["robinhood_trader", "mock", "test"]
+__all__ = ["robinhood_trader", "mock_trader", "test"]
 import os, sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import robinhood_trader
 from robinhood_trader import RobinhoodTrader
-import mock
-from mock import Account, MockTrader
+import mock_trader
+from mock_trader import Account, MockTrader
