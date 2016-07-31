afa28a0482964ec286e493a40ded5efd287d69dd
Ian Glen Neal <ian.gl.neal@gmail.com>
2016-07-30 18:21:04 -0700

    Fixing up some __init__.py problems that prevented packages from loading in the right way

--- robinhood/__init__.py
+++ robinhood/__init__.py
@@ -3,4 +3,3 @@ import os, sys
 sys.path.append(os.path.dirname(__file__))
 from robinhood_trader import RobinhoodTrader
 from mock import Account, MockTrader
-import test
--- robinhood/test/__init__.py
+++ robinhood/test/__init__.py
@@ -2,5 +2,3 @@ __package__ = "robinhood"
 __all__ = ["mock_test", "robinhood_trader_test"]
 import os, sys
 sys.path.append(os.path.dirname(__file__))
-import mock_test
-import robinhood_trader_test
