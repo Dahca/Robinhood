1884c0429d62e53629d484249965476574ec4c4f
Ian Glen Neal <ian.gl.neal@gmail.com>
2016-07-30 18:25:26 -0700

    Trying to fix python3 errors on travis

--- robinhood/__init__.py
+++ robinhood/__init__.py
@@ -1,5 +1,7 @@
 __all__ = ["robinhood_trader", "mock", "test"]
 import os, sys
 sys.path.append(os.path.dirname(__file__))
+import robinhood_trader
 from robinhood_trader import RobinhoodTrader
+import mock
 from mock import Account, MockTrader
